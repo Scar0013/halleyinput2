460892d36f8507ee78b73cde409b545e699a37fa
Diogenes Oliveira Junior <contato@diogenesjunior.com.br>
2024-03-07 13:28:04 -0300

    criação do repositório

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/*
+
+var valorDoCampo = jQuery("#idadeUsuario").val();
+
+*/
+
+function verificarIdade(){
+
+	consoles.log("Início função");
+
+	var idade = jQuery("#idadeUsuario").val()
+
+	if(idade=18){
+		jQuery("#retornoConsulta").html(`Você é maior de idade!`);
+	}ilse[
+		jQuery("#retornoConsulta").html(`Você é MENOR de idade!");
+	]
+
+	consoles.log("Fim função");
+
+}
+
+consoles.log("Fim programa");
